7951490ac74f6b15cc9eedc4f4dacebd9e696c00
amazing-username <kdeng02@aurora.edu>
2016-06-14 20:10:24 +0000

    Added some javafx sutff... Not runnable yet.

--- MoneyManager.java
+++ MoneyManager.java
@@ -1,9 +1,17 @@
 
 
 
+import javafx.application.Application;
 
-
-public class MoneyManager
+public class MoneyManager extends Application
 {
-
+	
+	public void start(Stage primaryStage)
+	{
+	
+	}
+	public static void main(String[] args)
+	{
+		launch(args);
+	}	
 }
