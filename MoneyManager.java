5bd557c1ecad4187577dc435682a65d3567eb8d8
amazing-username <kdeng02@aurora.edu>
2016-06-14 20:21:29 +0000

    Added some comments to the page

--- MoneyManager.java
+++ MoneyManager.java
@@ -1,4 +1,16 @@
-
+/*
+ * Programmer: Kun Deng
+ * Date created: June 14, 2016
+ * 
+ * Purpose: To create an application to manage money
+ * in different psuedo-accounts that are actually from
+ * a single account. Lets say I have a checking account
+ * with $500,000 and I have ten accounts. To keep a 
+ * record of the transactions from all those accounts
+ * I have to log everything down but with an application,
+ * I just have to add data and the data can be saved to
+ * prevent data lost, opposed to writing down to a ledger.
+ */
 
 
 import javafx.application.Application;
