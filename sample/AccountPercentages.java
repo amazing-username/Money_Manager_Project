95de9542f15ef58db606f7d00d9e5ea7908de6b2
amazing-username <kdeng02@aurora.edu>
2016-06-27 16:42:31 -0500

    Added functionality to the Edit Menu, specifically editing percentages.

---
+++ sample/AccountPercentages.java
@@ -0,0 +1,67 @@
+package sample;
+
+public class AccountPercentages
+{
+    public AccountPercentages()
+    {
+
+    }
+
+    private double iPhoneBatteryFundPercentage = .11;
+    private double personalEmergencyFundPercentage = .15;
+    private double familyEmergencyFundPercentage = .10;
+    private double carFundPercentage = .11;
+    private double investingFundPercentage = .09;
+    private double clothingFundPercentage = .12;
+    private double supplementFundPercentage = .08;
+    private double chessSetFundPercentage = .04;
+    private double runningFundPercentage = .10;
+    private double miscellaneousFundPercentage = .1;
+
+    public void setiPhoneBatteryFundPercentage(double percentage)
+    {
+        this.iPhoneBatteryFundPercentage = percentage;
+    }
+
+
+    public double getiPhoneBatteryFundPercentage()
+    {
+        return iPhoneBatteryFundPercentage;
+    }
+    public double getPersonalEmergencyFundPercentage()
+    {
+        return personalEmergencyFundPercentage;
+    }
+    public double getFamilyEmergencyFundPercentage()
+    {
+        return familyEmergencyFundPercentage;
+    }
+    public double getCarFundPercentage()
+    {
+        return carFundPercentage;
+    }
+    public double getInvestingFundPercentage()
+    {
+        return investingFundPercentage;
+    }
+    public double getClothingFundPercentage()
+    {
+        return clothingFundPercentage;
+    }
+    public double getSupplementFundPercentage()
+    {
+        return supplementFundPercentage;
+    }
+    public double getChessSetFundPercentage()
+    {
+        return chessSetFundPercentage;
+    }
+    public double getRunningFundPercentage()
+    {
+        return runningFundPercentage;
+    }
+    public double getMiscellaneousFundPercentage()
+    {
+        return miscellaneousFundPercentage;
+    }
+}
--- sample/MoneyManagerControls.java
+++ sample/MoneyManagerControls.java
@@ -1,9 +1,16 @@
 package sample;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 
 public class MoneyManagerControls
@@ -43,6 +50,10 @@ public class MoneyManagerControls
         addPaycheck.setText("add paycheck");
         manualMoneyTransfer.setText("transfer money");
         changePercentages.setText("change percentages");
+        changePercentages.setOnAction( e ->
+        {
+            newWindowToChangePercentages();
+        });
         mmEditMenu.getItems().addAll(addPaycheck, manualMoneyTransfer,
                 changePercentages);
 
@@ -56,6 +67,124 @@ public class MoneyManagerControls
         mmMenuBar.getMenus().addAll(mmFileMenu, mmEditMenu, mmHelpMenu);
 
         return mmMenuBar;
+    }
+    public void listCurrentPercentages(TextField iPhoneFundPercentage, TextField personalEmergencyFundPercentage,
+                                       TextField familyEmergencyFundPercentage, TextField carFundPercentage,
+           TextField investingFundPercentage, TextField clothingFundPercentage, TextField supplementFundPercentage,
+            TextField chessSetFundPercentage, TextField runningFundPercentage, TextField miscellaneousFundPercentage,
+                                       AccountPercentages percentageStorage)
+    {
+        iPhoneFundPercentage.setText("" + (percentageStorage.getiPhoneBatteryFundPercentage() * 100));
+        personalEmergencyFundPercentage.setText("" + (percentageStorage.getPersonalEmergencyFundPercentage() * 100));
+        familyEmergencyFundPercentage.setText("" + (percentageStorage.getFamilyEmergencyFundPercentage() * 100));
+        carFundPercentage.setText("" + (percentageStorage.getCarFundPercentage() * 100));
+        investingFundPercentage.setText("" + (percentageStorage.getInvestingFundPercentage() * 100));
+        clothingFundPercentage.setText("" + (percentageStorage.getClothingFundPercentage() * 100));
+        supplementFundPercentage.setText("" + (percentageStorage.getSupplementFundPercentage() * 100));
+        chessSetFundPercentage.setText("" + (percentageStorage.getChessSetFundPercentage() * 100));
+        runningFundPercentage.setText("" + (percentageStorage.getRunningFundPercentage() * 100));
+        miscellaneousFundPercentage.setText("" + (percentageStorage.getMiscellaneousFundPercentage() * 100));
+    }
+    public void newWindowToChangePercentages() {
+        Stage percentageStage = new Stage();
+        BorderPane percentageBorderPane = new BorderPane();
+        AccountPercentages percentageStorage = new AccountPercentages();
+
+
+        VBox percentageLabels = new VBox();
+
+        Label iPhoneFundPercentageLabel = new Label();
+        iPhoneFundPercentageLabel.setText("iPhone Percentage");
+        Label personalEmergencyFundPercentageLabel = new Label();
+        personalEmergencyFundPercentageLabel.setText("Personal Emergency Percentage");
+        Label familyEmergencyFundPercentageLabel = new Label();
+        familyEmergencyFundPercentageLabel.setText("Family Emergency Percentage");
+        Label carFundPercentageLabel = new Label("Car Percentage");
+        Label investingFundPercentageLabel = new Label("Investing Percentage");
+        Label clothingFundPercentageLabel = new Label("Clothing Percentage");
+        Label supplementFundPercentageLabel = new Label("Supplement Percentage");
+        Label chessSetFundPercentageLabel = new Label("Chess Set Percentage");
+        Label runningFundPercentageLabel = new Label("Running Percentage");
+        Label miscellaneousFundPercentageLabel = new Label("Miscellaneous Percentage");
+
+        percentageLabels.getChildren().addAll(iPhoneFundPercentageLabel, personalEmergencyFundPercentageLabel,
+                familyEmergencyFundPercentageLabel, carFundPercentageLabel, investingFundPercentageLabel,
+                clothingFundPercentageLabel, supplementFundPercentageLabel, chessSetFundPercentageLabel,
+                runningFundPercentageLabel, miscellaneousFundPercentageLabel);
+        percentageLabels.setPadding(new Insets(10, 10, 10, 10));
+        percentageLabels.setSpacing(20);
+
+        VBox percentageTextFields = new VBox();
+
+        TextField iPhoneFundPercentageTextField = new TextField();
+        iPhoneFundPercentageTextField.setPromptText("Enter Percentage");
+        TextField personalEmergencyFundPercentageTextField = new TextField();
+        personalEmergencyFundPercentageTextField.setPromptText("Enter Percentage");
+        TextField familyEmergencyFundPercentageTextField = new TextField();
+        familyEmergencyFundPercentageTextField.setPromptText("Enter Percentage");
+        TextField carFundPercentageTextField = new TextField();
+        carFundPercentageTextField.setPromptText("Enter Percentage");
+        TextField investingFundPercentageTextField = new TextField();
+        investingFundPercentageTextField.setPromptText("Enter Percentage");
+        TextField clothingFundPercentageTextField = new TextField();
+        clothingFundPercentageTextField.setPromptText("Enter Percentage");
+        TextField supplementFundPercentageTextField = new TextField();
+        supplementFundPercentageTextField.setPromptText("Enter Percentage");
+        TextField chessSetFundPercentageTextField = new TextField();
+        chessSetFundPercentageTextField.setPromptText("Enter Percentage");
+        TextField runningFundPercentageTextField = new TextField();
+        runningFundPercentageTextField.setPromptText("Enter Percentage");
+        TextField miscellaneousFundPercentageTextField = new TextField();
+        miscellaneousFundPercentageTextField.setPromptText("Enter Percentage");
+
+        percentageTextFields.getChildren().addAll(iPhoneFundPercentageTextField, personalEmergencyFundPercentageTextField,
+                familyEmergencyFundPercentageTextField, carFundPercentageTextField, investingFundPercentageTextField,
+                clothingFundPercentageTextField, supplementFundPercentageTextField, chessSetFundPercentageTextField,
+                runningFundPercentageTextField, miscellaneousFundPercentageTextField);
+        percentageTextFields.setPadding(new Insets(10, 10, 10, 10));
+        percentageTextFields.setSpacing(10);
+
+
+        Menu percentageEditMenu = new Menu("Edit");
+        MenuItem currentPercentages, updatePercentages, closePercentages;
+
+        currentPercentages = new MenuItem("Load Current Percentages");
+        currentPercentages.setOnAction(e ->
+        {
+            listCurrentPercentages(iPhoneFundPercentageTextField, personalEmergencyFundPercentageTextField,
+                    familyEmergencyFundPercentageTextField, carFundPercentageTextField, investingFundPercentageTextField,
+                    clothingFundPercentageTextField, supplementFundPercentageTextField, chessSetFundPercentageTextField,
+                    runningFundPercentageTextField, miscellaneousFundPercentageTextField, percentageStorage);
+        });
+        updatePercentages = new MenuItem("Update Percentages");
+        closePercentages = new MenuItem("Close Percentages Window");
+        closePercentages.setOnAction(e ->
+        {
+            percentageStage.close();
+        });
+
+        percentageEditMenu.getItems().addAll(currentPercentages, updatePercentages, closePercentages);
+
+        VBox percentageSign = new VBox();
+        for (int i = 0; i < 10; i++)
+        {
+            percentageSign.getChildren().add(i, new Label("%"));
+        }
+        percentageSign.setSpacing(21);
+        percentageSign.setPadding(new Insets(10, 10, 10, 10));
+
+        percentageBorderPane.setTop(new MenuBar(percentageEditMenu));
+        percentageBorderPane.setLeft(percentageLabels);
+        percentageBorderPane.setCenter(percentageTextFields);
+        percentageBorderPane.setRight(percentageSign);
+
+        Scene percentageScene = new Scene(percentageBorderPane);
+
+        //Stage percentageStage = new Stage();
+        percentageStage.setMinHeight(400);
+        percentageStage.setMinWidth(300);
+        percentageStage.setScene(percentageScene);
+        percentageStage.show();
     }
 
     public void setMoneyStuff(TableView moneyStuff)
