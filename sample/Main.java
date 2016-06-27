daaceaae1ac97f1c4ef03fcf621b0b24938904d7
amazing-username <kdeng02@aurora.edu>
2016-06-27 13:46:12 -0500

    Intellij git import (VCS)

--- sample/Main.java
+++ sample/Main.java
@@ -18,7 +18,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Menu;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Button;
